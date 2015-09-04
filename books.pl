dc8b699f9d1de8fbbd1a3c76daa517a4d77789fa
Stuart Mathews <stumathews@gmail.com>
2015-09-04 11:38:50 +0100

    first knowledgebase file

---
+++ books.pl
@@ -0,0 +1,9 @@
+books(becomming_a_better_programmer,pete_goodliffe).
+books(understanding_and_using_c_pointers,richard_m_reese).
+books(century_c_tips_from_the_new_school, ben_klemens).
+books(the_c_programming_language, brian_w_kernighan).
+books(the_c_programming_language, dennis_richie).
+books(c_5_pocket_reference_instant_help_for_c_5, joseph_albahari).
+books(spring_in_action, craig_walls).
+books(seven_languages_in_seven_weeks,bruce_a_tate).
+books(pattern-orientated_software_architecture_for_dummies,robert_hanmer).
